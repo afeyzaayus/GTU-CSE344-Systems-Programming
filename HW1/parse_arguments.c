32e3bec44ee4c32b42aec617bb0e1006559091e7
ayşe feyza serbest <aysefeyzaserbest@gmail.com>
2026-03-11 23:27:56 +0300

    i+=2

--- HW1/parse_arguments.c
+++ HW1/parse_arguments.c
@@ -20,8 +20,8 @@ void print_error(t_error error, char flag, char *arg){
             break;
 
         case ERR_BAD_FORMAT:
-            write(STDERR_FILENO, "ERROR! : Argument in unexpected format. -> ", 42);
-            write(STDERR_FILENO, arg, strlen(arg)); //
+            write(STDERR_FILENO, "ERROR! : Argument in unexpected format. -> ", 43);
+            write(STDERR_FILENO, arg, strlen(arg)); 
             write(STDERR_FILENO, "\n", 1);
             break;
 
@@ -37,7 +37,7 @@ void print_error(t_error error, char flag, char *arg){
             break;
     }
 }
-
+// w -> return 2, others -> return 1, invalid -> return 0
 int is_correct_flags(int i, char flag, char **argv, t_file *criteria) {
     switch (flag) {
         case 'w':
@@ -48,7 +48,6 @@ int is_correct_flags(int i, char flag, char **argv, t_file *criteria) {
             return 1;
             break;
         case 'b': // byte olmalı ya o durumda bir error bastırmalı mısın?
-            // atoi() ile string'i tam sayıya (integer) çeviriyoruz
             criteria->file_size = atoi(argv[i + 1]);
             return 1;
             break;
@@ -61,7 +60,6 @@ int is_correct_flags(int i, char flag, char **argv, t_file *criteria) {
             return 1;
             break;
         case 'l':
-            // atoi() ile string'i tam sayıya çeviriyoruz
             criteria->link_count = atoi(argv[i + 1]);
             return 1;
             break;
@@ -83,27 +81,24 @@ int parse_arguments(int argc, char **argv, t_file *criteria) {
                 print_error(ERR_NO_VALUE, flag, NULL);
                 return 0; 
             }
-            int res = is_correct_flags(i, flag, argv, criteria);
+            int res = is_correct_flags(i, flag, argv, criteria); // w -> return 2, others -> return 1, invalid -> return 0
             if (res == 1) has_criteria = 1;
             else if (res == 0) return 0;
-            // Değeri okuduğumuz için indeksi bir kez daha atlatıyoruz
-            i++; 
+            //i++; 
         } else {
             print_error(ERR_BAD_FORMAT, 0, argv[i]);
             return 0; 
         }
-        i++;
+        i+=2; // çalışmaza burayı i++ yapmayı unutma.
     }
 
     if (criteria->target_dir == NULL) {
         print_error(ERR_NO_TARGET_DIR, 0, NULL);
         return 0;
     }
-    
     if (!has_criteria) {
         print_error(ERR_NO_CRITERIA, 0, NULL);
         return 0;
     }
-
     return 1; 
 }
